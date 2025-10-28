55770ea48d645a7f10661f9d5318397e942dc441
Abhijit S Mudigonda <abhijit.mudigonda@gmail.com>
2025-10-28 00:56:59 -0500

    [TESTING] some changes to find-dihedral-N-chi

--- find-dihedral-N-chi.m
+++ find-dihedral-N-chi.m
@@ -6,16 +6,17 @@ M := GradedRingOfHMFs(F, 500);
 k := [1, 2];
 
 ideals := [N : N in IdealsUpTo(1000, F) | Norm(N) ge 195];
-for N in ideals do
+  print Norm(N);
   H := HeckeCharacterGroup(N, [1,2]);
   chis := [chi : chi in Elements(H) | Order(chi) eq 2 and IsCompatibleWeight(chi, k)];
   for chi in chis do
     Mk := HMFSpace(M, N, k, chi);
-    if #PossibleGrossenchars(Mk) gt 0 then
-      print IdealOneLine(N), HeckeCharLabel(chi), chi;
-      Sk := CuspFormBasis(Mk : StableOnly:=true);
-      print "possible grossenchars", #PossibleGrossenchars(Mk);
-      print "#Sk", #Sk;
+    num_gcs := #PossibleGrossenchars(Mk);
+    if num_gcs gt 0 then
+      print "Found:", LMFDBLabel(N), HeckeCharLabel(chi), chi, num_gcs;
+      // print Factorization(N);
+      // Sk := CuspFormBasis(Mk : StableOnly:=true);
+      // print "#Sk", #Sk;
     end if;
   end for;
 end for;
