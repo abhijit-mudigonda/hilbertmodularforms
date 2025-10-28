fb18abc03ef532abde6c6526b4e1fea8ccc67f9f
Abhijit S Mudigonda <abhijit.mudigonda@gmail.com>
2025-10-28 15:34:05 -0500

    [TESTING] more code to help verify rationality questions

--- blah.m
+++ blah.m
@@ -1,18 +1,34 @@
 load "config.m";
 
 SetVerbose("ModFrmHil", 3);
-PREC := 200;
-F := QuadraticField(5);
+PREC := 888;
+F := QuadraticField(2);
 ZF := Integers(F);
 
-N := 13*ZF;
-H := HeckeCharacterGroup(N, [1,2]);
-chi := H.1^6;
-assert Order(chi) eq 2;
-
 M := GradedRingOfHMFs(F, PREC);
-Mk := HMFSpace(M, N, [2, 2], chi);
-Sk := CuspFormBasis(Mk);
 
-M44 := HMFSpace(M, N, [4, 4]);
-S44 := CuspFormBasis(M44);
+full_label := "-2.0.1_119.2_2u1.0u1.2u";
+
+chi := FullChiLabelToHeckeChar(full_label);
+assert Order(chi) eq 2;
+N := Modulus(chi);
+k := [1,2];
+Mk := HMFSpace(M, N, k, chi);
+Sk := CuspFormBasis(Mk);
+assert #Sk eq 2;
+
+M24 := HMFSpace(M, N, [2,4]);
+S24 := CuspFormBasis(M24);
+assert #LinearDependence(S24) eq 0;
+
+Sk_squared := [Sk[1]^2, Sk[1] * Sk[2], Sk[2]^2];
+assert #Intersection(Sk_squared, S24) eq 3;
+
+eigs := Eigenbasis(Mk, Sk : P:=12, coprime_only:=false);
+dinv_F := IdealToRep(M, 1*ZF);
+f := eigs[1];
+f := f / Coefficient(f, 1*ZF, dinv_F);
+
+[#LinearDependence([f, HeckeOperator(f, pp)]) : pp in PrimesUpTo(20, F)];
+
+#Intersection([f^2], S24);
