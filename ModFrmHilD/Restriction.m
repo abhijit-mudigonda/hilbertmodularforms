ccff22f74d3a4c1b6420630e8c6444de27b59d33
Abhijit S Mudigonda <abhijit.mudigonda@gmail.com>
2026-08-29 16:45:29 -0500

    feat(Restriction): add AsCoefficients option to RestrictionToDiagonal
    Returns the SeqEnum of q-expansion coefficients of the restriction, in
    whatever ring they naturally live, instead of coercing into a classical
    ModularForms space (which only supports rational coefficients) -- also
    works when the restriction's coefficients are not rational.
    
    (Split out from the original "scripts" commit, which bundled this in
    with a batch of scratch output files and example scripts that don't
    belong in this history, and an unrelated default-parameter change to
    HeckeStabilityCuspBasis that's being reverted separately.)

--- ModFrmHilD/Restriction.m
+++ ModFrmHilD/Restriction.m
@@ -1,23 +1,32 @@
-intrinsic RestrictionToDiagonal(f::ModFrmHilDElt,M::ModFrmHilDGRng,bb::RngOrdIdl) -> ModFrmElt
-  {Given an HMF f of parallel weight k, returns the classical modular curve of weight nk and level obtained from restricting
-  the component bb of the HMF to the diagonal.}
+intrinsic RestrictionToDiagonal(f::ModFrmHilDElt,M::ModFrmHilDGRng,bb::RngOrdIdl : AsCoefficients:=false) -> Any
+  {Given an HMF f of parallel weight k, returns the classical modular form of weight nk and level obtained from
+  restricting the component bb of the HMF to the diagonal, as a ModFrmElt. If AsCoefficients is true, instead
+  returns the SeqEnum of q-expansion coefficients of the restriction, in whatever ring they naturally live,
+  without coercing into a classical ModularForms space (which only supports rational coefficients) -- this
+  also works when the restriction's coefficients are not rational.}
   require #SequenceToSet(Weight(f)) eq 1: "Only defined for parallel weight.";
   F := M`BaseField;
   ZF := Integers(F);
-  C := BaseField(F);
-  R<q> := PowerSeriesRing(C);
-  restriction := R!0;
   NN := Level(f);
   N := Integers()!(Denominator(NN)^(-1)*Generator((Denominator(NN)*NN) meet Integers()));
   D := Different(ZF);
   k :=  Weight(f)[1];
   n := #Weight(f);
   fbb := f`Components[bb];
-  // modForms only accepts integer coefficients
-  denom := 1;
   b := Integers()!(Denominator(bb)^(-1)*Generator((Denominator(bb)*bb) meet Integers()));
+
+  if not AsCoefficients then
+    C := BaseField(F);
+    R<q> := PowerSeriesRing(C);
+    restriction := R!0;
+    // modForms only accepts integer coefficients
+    denom := 1;
+  else
+    raw_coeffs := AssociativeArray();
+  end if;
+
   prec := 0;
-  for j in [0 .. Precision(fbb)] do 
+  for j in [0 .. Precision(fbb)] do
     tracej := PositiveElementsOfTrace(bb*D^(-1),j);
     coefficient := 0;
     for nu in tracej do
@@ -27,11 +36,27 @@ intrinsic RestrictionToDiagonal(f::ModFrmHilDElt,M::ModFrmHilDGRng,bb::RngOrdIdl
         break j;
       end if;
       coefficient +:= coeffNu;
-      denom := Lcm(denom, Denominator(coeffNu));
+      if not AsCoefficients then
+        denom := Lcm(denom, Denominator(coeffNu));
+      end if;
     end for;
-    restriction +:= coefficient*q^(j div b);
+    exp := j div b;
+    if AsCoefficients then
+      raw_coeffs[exp] := IsDefined(raw_coeffs, exp) select raw_coeffs[exp] + coefficient else coefficient;
+    else
+      restriction +:= coefficient*q^exp;
+    end if;
     prec +:= 1;
   end for;
+
+  if AsCoefficients then
+    if #Keys(raw_coeffs) eq 0 then
+      return [];
+    end if;
+    max_exp := Max(Keys(raw_coeffs));
+    return [IsDefined(raw_coeffs, e) select raw_coeffs[e] else 0 : e in [0 .. max_exp]];
+  end if;
+
   modForms := ModularForms(Gamma0(N),n*k);
   return modForms!(denom*(restriction +O(q^(prec))));
 end intrinsic;
